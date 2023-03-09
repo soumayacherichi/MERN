e87b195750909d300627938156a4ff428d450192
Soumaya cherichi <soumayacherichi@gmail.com>
2023-03-09 21:07:49 +0100

    person-card-button

--- React App/person-card-button/src/components/PersonCard.jsx
+++ React App/person-card-button/src/components/PersonCard.jsx
@@ -3,26 +3,24 @@ class PersonCard extends Component {
     constructor(props){
         super(props);
         this.state = {
-            // stateFavNumber: this.props.favNumber,
-            john:this.props.john
+            age:this.props.age
         }
     }
     render (){
-        const happyBirthDay = ()=>{
-            console.log("Clicked button 🤩🤗")
-            // this.state.stateFavNumber +=1
-            // this.setState({stateFavNumber:this.state.stateFavNumber +1},()=>{console.log(this.state.stateFavNumber)})
-            this.setState({john:{...this.state.john,age:this.state.john.age +=1}})
+        const happyBirthDay = ()=>
+        {
+            console.log("Clicked button 🤩🤗", this.state.age)
+            this.setState({age:this.state.age + 1})
             ;
         }
         return(
         <div>
             <h1>{this.props.last_Name}, {this.props.first_Name}</h1>
-            <p>Age: {this.props.age}</p>
+            <p>Age: {this.state.age}</p>
             <p>Hair Color: {this.props.hairColor}</p>
-            <button onClick={()=>{happyBirthDay()}}>Happy Birthday</button>
+            <button onClick={()=>happyBirthDay()}>Happy Birthday</button>
         </div>
         )
     }
 }
-    export default PersonCard;
+export default PersonCard;
