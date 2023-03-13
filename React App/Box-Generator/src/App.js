43910bb14a93de5b780dff39fffa3aa812a72d9f
Soumaya cherichi <soumayacherichi@gmail.com>
2023-03-13 20:27:33 +0100

    Box-Generator

---
+++ React App/Box-Generator/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import BoxContainer from './components/BoxContainer';
+function App() {
+ 
+  return ( 
+    <div className="App">
+      <fieldset>
+        <legend>App.js</legend>
+        <BoxContainer/>
+      </fieldset>
+    </div>
+  );
+}
+
+export default App;
