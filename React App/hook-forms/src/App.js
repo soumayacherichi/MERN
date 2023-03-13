e1580b03e2ecdf8807111c848c881675645a3633
Soumaya cherichi <soumayacherichi@gmail.com>
2023-03-13 04:58:46 +0100

    hook-forms

---
+++ React App/hook-forms/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Form from "./components/Form"
+
+function App() {
+  return (
+    <div className="App">
+      <fieldset>
+        <legend>
+          App.js
+        </legend>
+        <Form/>
+      </fieldset>
+    </div>
+  );
+}
+
+export default App;
