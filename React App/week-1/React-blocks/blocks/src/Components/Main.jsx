8649cde822a1d60d4eabfb25eb6b9a82f499e32e
Soumaya cherichi <soumayacherichi@gmail.com>
2023-03-19 09:28:19 +0100

    updated

---
+++ React App/week-1/React-blocks/blocks/src/Components/Main.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import styled from 'styled-components';
+const  Main = styled.div`
+        border: 1px solid lightgray;
+        background: #e06666;
+        width:'700px';
+        height:'600px';
+    `;
+export default Main
---
+++ React App/week-1/React-blocks/blocks/src/Components/Navigation.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import styled from 'styled-components';
+const  Navigation = styled.div`
+        border: 1px solid lightgray;
+        background: #6fa8dc;
+        width:'300px';
+        height:'600px';
+    `;
+export default Navigation
